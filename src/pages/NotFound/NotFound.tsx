2fdbc9c3e3841b283b09b5fda4acf3a8d610fa8f
mumumu <takumu.sato@gmail.com>
2025-02-26 15:42:23 +0900

    notfoundviewを修正

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,13 +1,18 @@
+import { Box } from "@mui/material";
 import Header from "../../components/Header";
+import { useTranslation } from "react-i18next";
 
 const NotFound = () => {
-    return (
-      <div>
-        <Header/>
+  const { t } = useTranslation();
+
+  return (
+    <Box sx={{ width: "100vw", height: "100vh", display: "flex", position: "relative" }}>
+      <Header />
+      <Box sx={{ margin: "auto" }}>
         <h1 className="text-2xl font-bold text-red-500">404 Not Found</h1>
-        <p>Oops! The page you are looking for does not exist.</p>
-      </div>
-    );
-  };
-  export default NotFound;
-  
+        <p>{t("Oops! The page you are looking for does not exist.")}</p>
+      </Box>
+    </Box>
+  );
+};
+export default NotFound;
