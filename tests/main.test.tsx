edcb2c96e72ecf4e1d0d9e33a880676852c29862
手塚悠貴 <yuki.tetsuka@hitsuzen.co.jp>
2025-03-19 16:48:34 +0900

    テストを作成

--- tests/main.test.tsx
+++ tests/main.test.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
-import { describe, it, vi, expect } from 'vitest';
-import '@/main';
+import { describe, it, vi, expect, type MockInstance } from 'vitest';
+import '../src/main';
 
 vi.mock('react-dom/client', () => ({
   createRoot: vi.fn(() => ({
@@ -15,7 +15,8 @@ describe('main.tsx', () => {
     expect(createRoot).toHaveBeenCalledWith(document.getElementById('root'));
 
     // `render` が `StrictMode` と `App` を含むコンポーネントを受け取っているか
-    const mockRender = (createRoot as vi.Mock).mock.results[0].value.render;
+    const mockRender = (createRoot as unknown as MockInstance).mock.results[0]
+      .value.render;
     expect(mockRender).toHaveBeenCalledTimes(1);
   });
 });
